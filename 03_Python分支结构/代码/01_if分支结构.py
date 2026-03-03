8ac5ad5555c18d413d40e9afbfe9cb2f1c5b48f6
yishazhao <zys15755433198@126.com>
2026-03-03 17:19:04 +0800

    Python branching structure

--- 03_Python分支结构/代码/01_if分支结构.py
+++ 03_Python分支结构/代码/01_if分支结构.py
@@ -1,14 +1,31 @@
 ''' '''
+# 三大结构
+# 1. 顺序结构：程序从上向下依次执行
+# 2. 分支结构
+# 3. 循环结构
+
+
 # if分支结构
 
 # Python语言有强制缩进
-
+# if True:
+# print("hello world")    # IndentationError: expected an indented block after 'if' statement on line 11
+if True:
+    print("hello world")
 
 # 单分支
-
+age = int(input("请输入年龄："))
+if age >= 18:
+    print("成年")
+print("end")
 
 # 双分支
-
+age = int(input("请输入年龄："))
+if age >= 18:
+    print("恭喜成年")
+else:
+    print("未成年小屁孩")
+print("end")
 
 # 多分支
 #  age范围
@@ -16,8 +33,15 @@
 #    18~30 : 年轻人
 #    30~60 : 中年人
 #    age>60 : 老年人
-
-
+age = int(input("请输入年龄："))
+if age < 18:
+    print("未成年")
+elif age <= 30:          # 这里的elif相当于C里面的else if
+    print("年轻人")
+elif age <= 60:
+    print("中年人")
+else:
+    print("老年人")
 
 
 
@@ -26,7 +50,15 @@
 #    0~3 : 婴儿
 #    3~6 ： 幼儿
 #    6~12 ：儿童
-
+age = int(input("请输入年龄："))
+if age < 0 or age > 12:
+    print("输入有误，要求输入0~12岁之间")
+elif age <= 3:
+    print("婴儿")
+elif age <= 6:
+    print("幼儿")
+else:
+    print("儿童")
 
 
 # 练习2：
@@ -34,7 +66,13 @@
 #    如果sex=='男'：输出王思聪
 #    如果sex=='女'：输出刘亦菲
 #    否则：输出泰国人
-
+sex = str(input("请输入性别："))
+if sex == '男':
+    print("帅哥")
+elif sex == '女':
+    print("美女")
+else:
+    print("跨性别者")
 
 
 
@@ -44,25 +82,51 @@
 #   1.年龄<=30
 #   2.身高>=1.75m
 #   3.年薪>=20w
-
+age = int(input("年龄多大了："))
+if age <= 30:
+    height = float(input("身高多高哇："))
+    if height >= 1.75:
+        salary = float(input("年薪多少呢："))
+        if salary >= 20:
+            print("那可以试试看!")
+        else:
+            print("年薪有点低...算了吧")
+    else:
+        print("身高不太高哇，不考虑了")
+else:
+    print("年龄有点大，不想见")
 
 
 
 # if 条件
 # bool值隐式判断
-
+if True:      # bool值隐式判断
+    print("10")
 
 
 
 # 扩展
 # 输入2个数，得到较大的数
+a = int(input("请输入a："))
+b = int(input("请输入b："))
+if a > b:
+    print(a)
+else:
+    print(b)
 
+# 一行表示：if-else
+print(a if a > b else b)
 
 
 # 练习：
 #   input("请说出你的心里话(喜欢/不喜欢):")
 #   如果喜欢，输出：小女子无以为报,只有以身相许
 #   如果不喜欢，输出：小女子无以为报,只有来世做牛做马报答公子大恩
+input("请说出你的心里话(喜欢/不喜欢):")
+if input == "喜欢":
+    print("小女子无以为报,只有以身相许")
+else :
+    print("小女子无以为报,只有来世做牛做马报答公子大恩")
 
 
 
@@ -72,6 +136,14 @@
 #    70<= score <90: B
 #    60<= score <70: C
 #    score < 60 : D
-
+score = int(input("请输入成绩："))
+if score >= 90:
+    print("A")
+elif score >= 70:
+    print("B")
+elif score >= 60:
+    print("C")
+else:
+    print("D")
 
 
--- 03_Python分支结构/代码/02_match-case分支语句.py
+++ 03_Python分支结构/代码/02_match-case分支语句.py
@@ -7,7 +7,28 @@
 
 # match-case : Python3.10及以上版本
 # 了解
+# 类似 switch-case  匹配的是一个固定的值，范围判断不太好用
 
+n = input("请输入您的等级（A/B/C/D）：")
+match n:
+    case 'A':                            # 字符类型，区分大小写，ASCII码值不同，A和a是不同的
+        print("成绩范围：>= 90")
+    case 'B':
+        print("成绩范围：70 ~ 90")
+    case 'C':
+        print("成绩范围：60 ~ 70")
+    case 'D':
+        print("成绩范围：< 60")
+    # 其他情况
+    case _:                        # _ 代表其他情况，相当于default，但是不用专门写break
+        print("输入不合法！")               
+'''
+Python 从设计上就规避了 case 穿透问题，执行完匹配到的分支就直接跳出 match 结构，不用专门写break
+    如果没有匹配到，会执行case _: 
+    如果没写 case _: 通配分支，且所有 case 都没匹配到，程序静默地跳过整个 match 结构继续执行后续代码，不报错且无输出。
 
+'''
 
-
+# switch-case穿透：
+# 简单来说就是当 case 分支里没有写 break 语句时，程序会继续执行后续 case 分支的代码，而不会跳出 switch 结构
+# 直到遇到 break 语句或者 switch 结构结束。
